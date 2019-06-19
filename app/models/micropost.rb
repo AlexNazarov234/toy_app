d258bb36e596589a57c87005c0ba6f4ba8b558e2
Alex Nazarov <pokupka91@mail.ru>
2019-06-19 12:33:52 +0300

    add validations for models

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
 	belongs_to :user
-	validates :content, length: { maximum: 140 }, presense: true
-	validates :user_id, presense: true
+	validates :content, length: { maximum: 140 }, presence: true
+	validates :user_id, presence: true
 end
